61987fc3af0749b859ac272d901ea2245955e208
Anttam <96652586+Anttam@users.noreply.github.com>
2024-01-04 16:22:47 -0500

    finished day 1

---
+++ Day 1/description.md
@@ -0,0 +1,2 @@
+# Day 1
+this project is a band name generator. The goal is to take the city you were born in and the name of your first pet and combine the two to give you a band name
---
+++ Day 1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city =input("What city were you born in?\n")
+pet = input("What is the name of your first pet\n")
+print("Your band name is"+" "+city+" "+pet)
